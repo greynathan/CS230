1345240196a29b497ba159a654dfa6c481a5749c
greynathan <ntg0006@mix.wvu.edu>
2020-09-20 20:03:10 -0400

    Lab3

---
+++ includes/dbhandler.php
@@ -0,0 +1,14 @@
+<?php
+
+$servename = "127.0.0.1";
+$DBuname = "root";
+$DBPass = "";
+$DBname = "cs230";
+
+$conn = mysqli_connect($servename, $DBuname, $DBPass, $DBname);
+
+if (!$conn) {
+    die("Connection failed...".mysqli_connect_error());
+    # code...
+}
+
